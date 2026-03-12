99b9c4ab7586a03304cac598aa1148dbcaa885ef
Govarthanan Selvaganessane <dotcreatives6222@gmail.com>
2026-03-12 11:28:53 +0530

    only open inmobile view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Govarthanan-Kavibharathi-Engagement';
+
+  isMobile = false;
+  isPortrait = false;
+
+  ngOnInit() {
+    this.checkDevice();
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    this.checkDevice();
+  }
+
+  checkDevice() {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+
+    this.isMobile = width <= 480;
+    this.isPortrait = height > width;
+  }
 }
